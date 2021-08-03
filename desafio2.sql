095a80579e65cb1e02f4c71e7372ea51631d02e2
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-03 19:09:44 -0300

    view que retorna a quantidade de cançoes, artistas e albuns do banco de dados

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(C.cancao_id) 'cancoes',
+COUNT(DISTINCT ALB.artista_id) 'artista',
+COUNT(DISTINCT ALB.album_id) 'albuns' 
+FROM SpotifyClone.cancoes C
+INNER JOIN SpotifyClone.albuns ALB
+ON ALB.album_id = C.album_id;
